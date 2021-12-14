7e5a0523129c9b02a1526021c332ee62953c539a
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-13 20:47:20 -0800

    Show.test.js - Test that when an item is selected, the handleSelect function is called

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -40,6 +40,13 @@ test('renders same number of options seasons are passed in', ()=>{
     expect(seasonOptions).toHaveLength(2);
 });
 
-test('handleSelect is called when an season is selected', () => {});
+test('handleSelect is called when an season is selected', () => {
+    const handleSelect = jest.fn();
+    render (<Show show = {testShow}  selectedSeason = {"none"}  handleSelect = {handleSelect} />);
+    const select = screen.getByLabelText(/Select A Season/i);
+    userEvent.selectOptions(select, ["1"]);
+
+    expect(handleSelect).toBeCalled();
+});
 
 test('component renders when no seasons are selected and when rerenders with a season passed in', () => {});
