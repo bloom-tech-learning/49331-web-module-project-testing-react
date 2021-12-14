7903cea6bec8d1e5a244300b00615779403f1963
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-13 19:06:06 -0800

    Episode.test.js - renders without error: pass testEpisode

--- src/components/Episodes.js
+++ src/components/Episodes.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Episode from './Episode';
 
 export default function Episodes(props) {
+  console.log('props in Episodes: ', props);
   return (
     <div data-testid="episodes-container" className="episodes">
       {props.episodes.map(episode => <Episode episode={episode}/>)}
--- src/components/tests/Episode.test.js
+++ src/components/tests/Episode.test.js
@@ -3,8 +3,19 @@ import '@testing-library/jest-dom/extend-expect';
 import { render, screen } from '@testing-library/react';
 import Episode from './../Episode';
 
+const testEpisode = {
+    id:1,
+    name: "",
+    image: "http://static.tvmaze.com/uploads/images/medium_landscape/67/168918.jpg",
+    season: 1,
+    number: 1,
+    summary: "",
+    runtime: 1
+}
 
-test("renders without error", () => {});
+test("renders without error", () => {
+    render (<Episode episode = { testEpisode }/>);
+});
 
 test("renders the summary test passed as prop", ()=>{});
 
