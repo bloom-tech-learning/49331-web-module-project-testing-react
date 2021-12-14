5c8eb039f6a3e19c879f6c2b4c09cb9e18ad186e
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-13 20:43:26 -0800

    Show.test.js - Test that the Loading component displays

--- src/components/tests/Show.test.js
+++ src/components/tests/Show.test.js
@@ -27,7 +27,11 @@ test('renders without errors', ()=>{
     render (<Show show = {testShow}  selectedSeason = { "none" } />)
 });
 
-test('renders Loading component when prop show is null', () => {});
+test('renders Loading component when prop show is null', () => {
+    render (<Show show= { null} />);
+    const loading = screen.queryByTestId("loading-container");
+    expect(loading).toBeInTheDocument();
+});
 
 
 test('renders same number of options seasons are passed in', ()=>{});
