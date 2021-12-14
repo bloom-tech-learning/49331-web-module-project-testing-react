680d405109fbe31da6997d11d4cded5c0ccfcdd7
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-12-13 21:09:44 -0800

    Display.test.js - Test that when the fetch button is pressed, the show component will display.

--- src/components/tests/Display.test.js
+++ src/components/tests/Display.test.js
@@ -5,10 +5,40 @@ import userEvent from '@testing-library/user-event';
 
 import Display from './../Display';
 
-test('renders without errors with no props', ()=>{ 
+import mockFetchShow from './../../api/fetchShow'
+jest.mock('./../../api/fetchShow')
+
+const testShow = {
+    //add in approprate test data structure here.
+    name: "test show",
+    summary: "test summary",
+    seasons: [
+        {
+            id: 0,
+            name: "season 1",
+            episodes: []
+        },
+        {
+            id: 1,
+            name: "season 2",
+            episodes: []
+        }
+    ]
+}
+
+test('renders without errors with no props',  ()=>{ 
     render (<Display />);
 });
 
-test('renders Show component when the button is clicked ', ()=>{});
+test('renders Show component when the button is clicked ', async ()=>{
+    mockFetchShow.mockResolvedValueOnce(testShow);
+
+    render (<Display />);
+    const button = screen.queryByRole("button");
+    userEvent.click(button);
+
+    const show = await screen.findByTestId("show-container");
+    expect(show).toBeInTheDocument();
+});
 
 test('renders show season options matching your data when the button is clicked', ()=>{});
